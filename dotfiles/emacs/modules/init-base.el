090a5936ae577a26ee5717f30e75d92598a2d88f
nobbele <realnobbele@gmail.com>
2025-09-19 00:27:21 +0200

    Fix emacs lsp

--- dotfiles/emacs/modules/init-base.el
+++ dotfiles/emacs/modules/init-base.el
@@ -27,15 +27,5 @@
 (use-package psession
   :ensure t
   :hook (after-init . psession-mode))
-
-(use-package lsp-mode
-  :init
-  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-  (setq lsp-keymap-prefix "C-c l")
-  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-         (XXX-mode . lsp)
-         ;; if you want which-key integration
-         (lsp-mode . lsp-enable-which-key-integration))
-  :commands lsp)
-
+  
 (provide 'init-base)
