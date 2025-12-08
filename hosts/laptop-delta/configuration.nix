0c488350865a84395caa2d1985edc587cbf9ba79
nobbele <realnobbele@gmail.com>
2025-12-08 11:33:17 +0100

    Use nixos-hardware

--- hosts/laptop-delta/configuration.nix
+++ hosts/laptop-delta/configuration.nix
@@ -3,9 +3,11 @@
   inputs,
   ...
 }: {
+  imports = [
+    inputs.nixos-hardware.nixosModules.lenovo-ideapad-14acn6
+  ];
   # me.tools.container.enable = true;
 
-  me.hardware.laptop.enable = true;
   me.browser.enable = true;
   me.audio.lowLatency = true;
   me.ssh.server.enable = false;
@@ -18,6 +20,26 @@
       command_prefix = '''';
     })
   ];
+
+  services.power-profiles-daemon.enable = false;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 60;
+
+      # START_CHARGE_THRESH_BAT1 = 40;
+      STOP_CHARGE_THRESH_BAT1 = 85;
+    };
+  };
 
   hardware.bluetooth.enable = true;
 
