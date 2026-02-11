a800a316ed0afe02cfce4818b4c6ee9bfeaf412c
nobbele <realnobbele@gmail.com>
2026-02-11 16:41:48 +0100

    Fix yubikey on beta (using HM)

--- hosts/desktop-beta/home.nix
+++ hosts/desktop-beta/home.nix
@@ -14,6 +14,30 @@
     creative.enable = true;
   };
 
+  programs.gpg = {
+    enable = true;
+  };
+
+  # TODO Move gpg-agent into a hm module and share it with the other computers.
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+    enableZshIntegration = true;
+    defaultCacheTtl = 3600;
+    maxCacheTtl = 999999;
+    pinentry.package = pkgs.pinentry-qt;
+  };
+
+  # I think this is unncessary? services.gpg-agent should already set it
+  home.sessionVariables = {
+    SSH_AUTH_SOCK = "$(${pkgs.gnupg}/bin/gpgconf --list-dirs agent-ssh-socket)";
+  };
+
+  # I think this is unncessary? services.gpg-agent should already set it
+  programs.zsh.initExtra = ''
+    export SSH_AUTH_SOCK="$(${pkgs.gnupg}/bin/gpgconf --list-dirs agent-ssh-socket)"
+  '';
+
   home.packages = with pkgs; [
     # More stable than installing it from the AUR :P
     paru
